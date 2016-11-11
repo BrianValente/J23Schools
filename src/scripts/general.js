049a6e139c183745a1b040484b14f7cbdb1caab6
Brian Valente <briannvalente@gmail.com>
2016-11-11 14:08:39 -0300

    New changes

--- src/scripts/general.js
+++ src/scripts/general.js
@@ -1,10 +1,18 @@
-var header_menu     = document.getElementById("header_menu");
-var sidebar         = document.getElementById("sidebar");
-var sidebarCloseBtn = document.getElementById("sidebar_header_close");
+var header_menu                = document.getElementById("header_menu");
+var sidebar                    = document.getElementById("sidebar");
+var sidebarCloseBtn            = document.getElementById("sidebar_header_close");
+var sidebarItemsSection        = document.querySelectorAll("#sidebar .section");
+var sidebarItemsSectionHeaders = document.querySelectorAll("#sidebar .section .header");
 
 
 function toggleSidebar() {
 	sidebar.classList.toggle("hide");
+}
+
+function hideAllSections() {
+	for (var i = 0; i<sidebarItemsSection.length; i++) {
+		sidebarItemsSection[i].classList.remove("show");
+	}
 }
 
 sidebarCloseBtn.onclick = toggleSidebar;
@@ -18,3 +26,10 @@ document.body.onscroll = function() {
 	var alpha = scroll / complete;
 	headerSolidBackground.style.opacity = alpha;
 };
+
+for (var i = 0; i<sidebarItemsSectionHeaders.length; i++) {
+	sidebarItemsSectionHeaders[i].addEventListener("click", function(item) {
+		if (!item.target.parentElement.classList.contains("show")) hideAllSections();
+		item.target.parentElement.classList.toggle("show");
+	});
+}
